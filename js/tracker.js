cc0875f3e5902c111b67cfcc04d47dbb8c704d69
dennis <dh7qc@mst.edu>
2016-11-01 00:55:51 -0500

    Began implementing the map and progress bar.

--- js/tracker.js
+++ js/tracker.js
@@ -15,4 +15,35 @@
    * Your implementation goes here!
    */
 
+  // URLs for the position and target.
+  let positionURL = trackerURL + '/position.json';
+  let targetURL = trackerURL + '/target.json';
+
+  // Get the destination info from website.
+  $.getJSON(targetURL, function(data) {
+    console.log(data);
+  });
+
+  // Initialize the map.
+  var mymap = L.map('map').setView(rollaCenter, 13);
+  L.tileLayer(osmUrl, osmAttrib).addTo(mymap);
+
+  // Get the position info from website.
+
+  // Add location icon.
+  var myIcon = L.icon({
+    iconUrl: meowthImageURL
+  });
+
+  // Do stuff with the current location.
+  $.getJSON(positionURL, function(data) {
+    let marker = L.marker([data['Lat'], data['Long']], {icon: myIcon}).addTo(mymap);
+    let pBar = document.getElementById('pBar');
+
+    document.getElementById('transportName').innerHTML = data['Transport'];
+    pBar.innerHTML = String(Number(data['Progress'] * 100).toPrecision(4)) + '%';  	 
+    pBar.style.width = data['Progress'] * 100 + '%';
+  });
+
+
 })();
